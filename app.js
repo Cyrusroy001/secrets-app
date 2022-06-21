007295801a4eb6b90dd9419321b2c0a66a09b11c
cyrusmew <51240567+Cyrusroy001@users.noreply.github.com>
2022-06-21 10:18:55 +0530

    use md5 hashing function to encrypt user passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -17,14 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-// should be before defining the model
-userSchema.plugin(encrypt, 
-    {
-        secret: process.env.SECRET,
-        encryptedFields: ["password"]
-    }
-);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +35,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     // encryption happens when save runs
     newUser.save(function(err) {
@@ -55,7 +47,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     // decryption happens when find runs
     User.findOne(
